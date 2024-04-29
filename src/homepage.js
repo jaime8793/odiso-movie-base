105f3a59c1d9dfb0eeebb5a36d78d3338a43be79
jaime <jamesowiti24@gmail.com>
2024-04-29 12:43:34 +0300

    Day 4

--- src/homepage.js
+++ src/homepage.js
@@ -4,6 +4,9 @@ import MoviePage from "./moviePage";
 function Homepage() {
   return (
     <div>
+      <div class="container-jagged">
+        <h1>Content Here</h1>
+      </div>
       <div class="second-header">
         <p>The best thing that happened to movies search history</p>
       </div>
