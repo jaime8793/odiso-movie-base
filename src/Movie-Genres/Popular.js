92f1de7f38d13ed32bc2c8ecb523e681ce02d7c4
jaime <jamesowiti24@gmail.com>
2024-05-30 22:30:53 +0300

    Will Be back

--- src/Movie-Genres/Popular.js
+++ src/Movie-Genres/Popular.js
@@ -6,8 +6,22 @@ function Popular() {
       <div>
         <p class="movie-page-text">Popular Movie's right now</p>
       </div>
+
       <div>
         <FetchDatas category="/trending" genre="/movie" day="/day" />
+      </div>
+      <div>
+        <iframe
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+          width="100%"
+          height="100%"
+          allowfullscreen="true"
+          marginheight="0"
+          marginwidth="0"
+          scrolling="no"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   );
